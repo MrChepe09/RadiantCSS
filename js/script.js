aa4d091417e2066e304f6a835bc78f507b14310e
Bhupinder Singh <khanujabhupinder09@gmail.com>
2021-04-02 21:40:31 +0530

    Added Files - Work in Progress

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+
+const sidebarBtn = document.querySelector('.sidebar-btn');
+const sidebar = document.querySelector('.sidebar')
+
+sidebarBtn.addEventListener('click', function() {
+    console.log(sidebar);
+    console.log(sidebarBtn);
+    sidebar.classList.toggle('sidebar-hide')
+})
+
+modalBtn.addEventListener('click', function() {
+    modal.classList.add('bg-active');
+})
+
+modalClose.addEventListener('click', function() {
+    modal.classList.remove('bg-active');
+})
