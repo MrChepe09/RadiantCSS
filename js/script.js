497d42e804eb9c3aa0f25c438b9ed6fbb3395d9f
Bhupinder Singh <khanujabhupinder09@gmail.com>
2021-04-06 16:43:56 +0530

    Completed Components Pages

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const modalBtn = document.querySelector('.modal-btn');
 const modal = document.querySelector('.modal-bg');
 const modalClose = document.querySelector('.modal-close');
+// const iframe = document.querySelector('iframe');
 
 const sidebarBtn = document.querySelector('.sidebar-btn');
 const sidebar = document.querySelector('.sidebar')
 
+// iframe.width = '200px';
+
 sidebarBtn.addEventListener('click', function() {
-    console.log(sidebar);
-    console.log(sidebarBtn);
+    // console.log(sidebar);
+    // console.log(sidebarBtn);
     sidebar.classList.toggle('sidebar-hide')
+    sidebar.classList.toggle('sidebar-show')
 })
 
 modalBtn.addEventListener('click', function() {
